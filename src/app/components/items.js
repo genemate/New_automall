d670fc23d9aad6eb41a7bc3a2f257a4605864ce3
genemate <110731423+genemate@users.noreply.github.com>
2023-12-18 18:37:17 +0500

    1

--- src/app/components/items.js
+++ src/app/components/items.js
@@ -2,102 +2,6 @@
 
 export const items = [
   {
-    _id: 1,
-    type: "Elektromobil",
-    name: "BYD Song Plus Champion",
-    price: "221 238 900",
-    stars: 3.5,
-    image: "/images/carSlider/Champion_car.png",
-    logo: "icons/brands/byd.svg",
-    backgroundImage: "/images/carSlider/Champion_bg.png",
-    info: [
-      {
-        icon: "icons/carSlider/engine.svg",
-        text: "38,8 kVt",
-      },
-      {
-        icon: "icons/carSlider/gas.svg",
-        text: "Elektr",
-      },
-      {
-        icon: "icons/carSlider/gearshift.svg",
-        text: "Avtomat",
-      },
-      {
-        icon: "icons/carSlider/wheel.svg",
-        text: "Old",
-      },
-      {
-        icon: "icons/carSlider/seat.svg",
-        text: "4 o'rindiq",
-      },
-    ],
-  },
-  {
-    id: 2,
-    type: "SUV",
-    name: "Le L9 Max",
-    price: "25 000 000 000",
-    stars: 5,
-    image: "/images/carSlider/Li_car.png",
-    logo: "icons/brands/logo_li.svg",
-    backgroundImage: "/images/carSlider/Li_bg.png",
-    info: [
-      {
-        icon: "icons/carSlider/engine.svg",
-        text: "1600 ЛС",
-      },
-      {
-        icon: "icons/carSlider/gas.svg",
-        text: "Газ",
-      },
-      {
-        icon: "icons/carSlider/gearshift.svg",
-        text: "Автомат",
-      },
-      {
-        icon: "icons/carSlider/wheel.svg",
-        text: "Передний",
-      },
-      {
-        icon: "icons/carSlider/seat.svg",
-        text: "5 мест",
-      },
-    ],
-  },
-  {
-    id: 3,
-    type: "Sedan",
-    name: "Chevrolet Monza",
-    price: "32 000 000 000",
-    stars: 4.9,
-    image: "/images/carSlider/Monza1.png",
-    logo: "icons/brands/chevrolet1.svg",
-    backgroundImage: "/images/carSlider/Monza_bg.png",
-    info: [
-      {
-        icon: "icons/carSlider/engine.svg",
-        text: "1600 ЛС",
-      },
-      {
-        icon: "icons/carSlider/gas.svg",
-        text: "Газ",
-      },
-      {
-        icon: "icons/carSlider/gearshift.svg",
-        text: "Автомат",
-      },
-      {
-        icon: "icons/carSlider/wheel.svg",
-        text: "Передний",
-      },
-      {
-        icon: "icons/carSlider/seat.svg",
-        text: "5 мест",
-      },
-    ],
-  },
-  {
     id: 4,
     type: "Sedan",
     name: "Honda Crider",
@@ -170,6 +74,102 @@ export const items = [
     image: "/images/carSlider/bmw.png",
     logo: "icons/brands/bmw_logo.svg",
     backgroundImage: "/images/carSlider/Mercedes_bg.png",
+    info: [
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "1600 ЛС",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Газ",
+      },
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Автомат",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Передний",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "5 мест",
+      },
+    ],
+  },
+  {
+    _id: 1,
+    type: "Elektromobil",
+    name: "BYD Song Plus Champion",
+    price: "221 238 900",
+    stars: 3.5,
+    image: "/images/carSlider/Champion_car.png",
+    logo: "icons/brands/byd.svg",
+    backgroundImage: "/images/carSlider/Champion_bg.png",
+    info: [
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "38,8 kVt",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Elektr",
+      },
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Avtomat",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Old",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "4 o'rindiq",
+      },
+    ],
+  },
+  {
+    id: 2,
+    type: "SUV",
+    name: "Le L9 Max",
+    price: "25 000 000 000",
+    stars: 5,
+    image: "/images/carSlider/Li_car.png",
+    logo: "icons/brands/logo_li.svg",
+    backgroundImage: "/images/carSlider/Li_bg.png",
+    info: [
+      {
+        icon: "icons/carSlider/engine.svg",
+        text: "1600 ЛС",
+      },
+      {
+        icon: "icons/carSlider/gas.svg",
+        text: "Газ",
+      },
+      {
+        icon: "icons/carSlider/gearshift.svg",
+        text: "Автомат",
+      },
+      {
+        icon: "icons/carSlider/wheel.svg",
+        text: "Передний",
+      },
+      {
+        icon: "icons/carSlider/seat.svg",
+        text: "5 мест",
+      },
+    ],
+  },
+  {
+    id: 3,
+    type: "Sedan",
+    name: "Chevrolet Monza",
+    price: "32 000 000 000",
+    stars: 4.9,
+    image: "/images/carSlider/Monza1.png",
+    logo: "icons/brands/chevrolet1.svg",
+    backgroundImage: "/images/carSlider/Monza_bg.png",
     info: [
       {
         icon: "icons/carSlider/engine.svg",
